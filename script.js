b81773056cb07296f1fee7734461e32a29d2e9e0
Haarit <haarit1905@gmail.com>
2024-11-29 10:07:35 +0530

    Final Template is Ready

--- script.js
+++ script.js
@@ -17,3 +17,38 @@ function opentab(tabname){
     document.getElementById(tabname).classList.add("active-tab");
 }
 
+
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemenu.style.right = "0";
+}
+
+function closemenu(){
+    sidemenu.style.right = "-250px";
+}
+
+
+// Deployment ID
+// AKfycbx1oF_Ro8jjxgjWFnYqnuFXhwRUxx6FmbnG8JrefCk38CEMbMVAfiEMyC1qk0-1BKez
+
+// Web App Url
+// https://script.google.com/macros/s/AKfycbx1oF_Ro8jjxgjWFnYqnuFXhwRUxx6FmbnG8JrefCk38CEMbMVAfiEMyC1qk0-1BKez/exec
+
+// See how to use google sheets to send the data
+// https://github.com/jamiewilson/form-to-google-sheets
+
+const scriptURL = "https://script.google.com/macros/s/AKfycbx1oF_Ro8jjxgjWFnYqnuFXhwRUxx6FmbnG8JrefCk38CEMbMVAfiEMyC1qk0-1BKez/exec"
+const form = document.forms['submit-to-google-sheet']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then((response) => {
+        alert("Succesfuly submitted the form");
+        document.getElementById('submit-to-google-sheet').reset();
+        console.log('Success!', response);
+    })
+    .catch(error => console.error('Error!', error.message))
+})
